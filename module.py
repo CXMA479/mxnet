8864b46f42ba346feba9db974e1b45e566db6116
yongliang chen <chen_yong_liang@cqu.edu.cn>
2017-10-23 21:51:42 +0800

    1) acc_fit, 2) use element-wise copy for grad

--- module.py
+++ module.py
@@ -13,6 +13,8 @@ this file is derived from one of the official examples, additional features:
 TODO:
 1. interface for load like *mx.mod.Module.load*
 
+CHANGELOG:
+    1. provides acc_fit taking mod as arg --Oct 22, 2017
 
 Chen Y. Liang
 """
@@ -21,11 +23,150 @@ Chen Y. Liang
 
 
 import logging
-
+import time
 from mxnet import context as ctx
 from mxnet.initializer import Uniform
 from mxnet.module.base_module import BaseModule
 from mxnet.module.module import Module
+import mxnet as mx
+metric = mx.metric
+BatchEndParam = mx.model.BatchEndParam
+_as_list = mx.base._as_list
+
+def acc_grad_arrays(mod, arg_acc_grad_arrays=None):
+    """
+        if grads is None, return the copied one else, return accumulated one
+    """
+    if arg_acc_grad_arrays is None:
+        return [ [grad.copy() if grad is not None else None for grad in grads]\
+                    for grads in mod._exec_group.grad_arrays  ]
+    else:
+        # accumulate...
+        return [ [grad + acc_grad if grad is not None else None for\
+                                    grad, acc_grad in zip(grads, acc_grads) ]\
+                            for grads, acc_grads in zip(mod._exec_group.grad_arrays, arg_acc_grad_arrays) ]
+
+def set_grad_arrays(mod, arg_acc_grad_arrays, normsize=1):
+    for acc_grads, mod_grads in zip(arg_acc_grad_arrays, mod._exec_group.grad_arrays):
+        for acc_grad, mod_grad in zip(acc_grads, mod_grads):
+            if acc_grad is not None:
+                mod_grad[:] = acc_grad[:]/normsize
+#    mod._exec_group.grad_arrays=[[grad.copyto(grad.context)/normsize if grad is not None else None\
+#                    for grad in grads] for grads in arg_acc_grad_arrays]
+
+
+def acc_fit(mod, update_batch_size,\
+            train_data, eval_data=None, eval_metric='acc',
+            epoch_end_callback=None, batch_end_callback=None, kvstore='local',
+            optimizer='sgd', optimizer_params=(('learning_rate', 0.01),),
+            eval_end_callback=None,
+            eval_batch_end_callback=None, initializer=Uniform(0.01),
+            arg_params=None, aux_params=None, allow_missing=False,
+            force_rebind=False, force_init=False, begin_epoch=0, num_epoch=None,
+            validation_metric=None, monitor=None):
+        """
+    this function aims to support training in larger input size by
+                allocating additive space to store auxiliary grads
+        mod:
+            mx.mod.Module
+        update_batch:
+            int, specifying how many batches between two updates
+        **arg_keys:
+            same as mod.fit
+        """
+        assert num_epoch is not None, 'please specify number of epochs'
+        it_batch_size=train_data.batch_size
+
+        mod.bind(data_shapes=train_data.provide_data, label_shapes=train_data.provide_label,
+                  for_training=True, force_rebind=force_rebind)
+        if monitor is not None:
+            mod.install_monitor(monitor)
+        mod.init_params(initializer=initializer, arg_params=arg_params, aux_params=aux_params,
+                         allow_missing=allow_missing, force_init=force_init)
+        mod.init_optimizer(kvstore=kvstore, optimizer=optimizer,
+                            optimizer_params=optimizer_params)
+
+        if validation_metric is None:
+            validation_metric = eval_metric
+        if not isinstance(eval_metric, metric.EvalMetric):
+            eval_metric = metric.create(eval_metric)
+
+        ################################################################################
+        # training loop
+        ################################################################################
+        arg_acc_grad_arrays=None # to store auxiliary grad_arrays
+        for epoch in range(begin_epoch, num_epoch):
+            tic = time.time()
+            eval_metric.reset()
+            nbatch = 0
+            data_iter = iter(train_data)
+            end_of_batch = False
+            next_data_batch = next(data_iter)
+            while not end_of_batch:
+                data_batch = next_data_batch
+                if monitor is not None:
+                    monitor.tic()
+                mod.forward_backward(data_batch)
+                arg_acc_grad_arrays = acc_grad_arrays(mod, arg_acc_grad_arrays)
+                if nbatch*it_batch_size % update_batch_size==0 and nbatch >0:
+                    set_grad_arrays(mod, arg_acc_grad_arrays, update_batch_size/it_batch_size) # normsize=1 by default(softmax norm)
+                    mod.update()
+                    arg_acc_grad_arrays=None
+                try:
+                    # pre fetch next batch
+                    next_data_batch = next(data_iter)
+                    mod.prepare(next_data_batch)
+                except StopIteration:
+                    end_of_batch = True
+
+                mod.update_metric(eval_metric, data_batch.label)
+
+                if monitor is not None:
+                    monitor.toc_print()
+
+                if batch_end_callback is not None:
+                    batch_end_params = BatchEndParam(epoch=epoch, nbatch=nbatch,
+                                                     eval_metric=eval_metric,
+                                                     locals=locals())
+                    for callback in _as_list(batch_end_callback):
+                        callback(batch_end_params)
+                nbatch += 1
+            if arg_acc_grad_arrays is not None:# left one update...
+                set_grad_arrays(mod, arg_acc_grad_arrays, update_batch_size/it_batch_size)
+                mod.update()
+                arg_acc_grad_arrays = None
+
+            # one epoch of training is finished
+            for name, val in eval_metric.get_name_value():
+                mod.logger.info('Epoch[%d] Train-%s=%f', epoch, name, val)
+            toc = time.time()
+            mod.logger.info('Epoch[%d] Time cost=%.3f', epoch, (toc-tic))
+
+            # sync aux params across devices
+            arg_params, aux_params = mod.get_params()
+            mod.set_params(arg_params, aux_params)
+
+            if epoch_end_callback is not None:
+                for callback in _as_list(epoch_end_callback):
+                    callback(epoch, mod.symbol, arg_params, aux_params)
+
+            #----------------------------------------
+            # evaluation on validation set
+            if eval_data:
+                res = mod.score(eval_data, validation_metric,
+                                 score_end_callback=eval_end_callback,
+                                 batch_end_callback=eval_batch_end_callback, epoch=epoch)
+                #TODO: pull this into default
+                for name, val in res:
+                    mod.logger.info('Epoch[%d] Validation-%s=%f', epoch, name, val)
+
+            # end of 1 epoch, reset the data-iter for another epoch
+            train_data.reset()
+
+
+
+
+
 
 class MutableModule(BaseModule):
     """A mutable module is a module that supports variable input data.
@@ -244,9 +385,8 @@ class MutableModule(BaseModule):
         for acc_grads, mod_grads in zip(self.grad,self._curr_module._exec_group.grad_arrays):
             for acc_grad, mod_grad in zip(acc_grads, mod_grads):
                 if acc_grad is not None:
-                    mod_grad = acc_grad.copyto(mod_grad.context)/normsize
-                else:
-                    mod_grad = None
+                    mod_grad[:] = acc_grad[:]/normsize
+
 #        try:
 #            del(self._curr_module._exec_group.execs[0].grad_arrays)
 #        except:
