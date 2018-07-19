8f06bf4849624f9ffcab002683a8ba047b69f939
feuker <chenyongliang.03@bytedance.com>
2018-07-19 17:32:30 +0800

    1. Fix beug in __main__ test when running append operation 2. rewrite for table indent * nameRecordIO.py

--- nameRecordIO.py
+++ nameRecordIO.py
@@ -8,93 +8,90 @@ import mxnet as mx
 import cPickle as cpk
 import os
 class  nameRecordIO(object):
+    """
+    1. get your data directly by its name!
+
+    rec.write_name(name,data)
+    data=rec.read_name(name)
+
+    2. appending operation is supported!
+        nameRecordIO(idx_path, uri, 'a' )
   """
-  1. get your data directly by its name!
+    def __init__(self, idx_path, uri, flag, key_type=int):
+        assert '.' in idx_path
+        self.file_path=idx_path.split('.')[0]
+        self.cpk_path=self.file_path+'.cpk'
+        if os.path.isfile(self.cpk_path):  # load it
+            f=open(self.cpk_path,'r')
+            try:
+                self.nameDict = cpk.load(f)
+            except: # if empty, len(rec.idx.keys() ) == 0
+                self.nameDict = {}
 
-      rec.write_name(name,data)
-      data=rec.read_name(name)
+            assert  type(self.nameDict) == dict,'%s not a dict!'%self.cpk_path
+            f.close()
+          # how many has been record...
+            self.count = len(self.nameDict.keys())
+        else:
+          # make a new file, dict...
+            self.count = 0
+            self.nameDict = {}
 
-  2. appending operation is supported!
-      nameRecordIO(idx_path, uri, 'a' )
-  """
-  def __init__(self, idx_path, uri, flag, key_type=int):
-    assert '.' in idx_path
-    self.file_path=idx_path.split('.')[0]
-    self.cpk_path=self.file_path+'.cpk'
-    if os.path.isfile(self.cpk_path):  # load it
-      f=open(self.cpk_path,'r')
-      try:
-         self.nameDict = cpk.load(f)
-      except: # if empty, len(rec.idx.keys() ) == 0
-         self.nameDict = {}
+        self.idx_path = idx_path
+        self.uri      = uri
 
-      assert  type(self.nameDict) == dict,'%s not a dict!'%self.cpk_path
-      f.close()
-      # how many has been record...
-      self.count = len(self.nameDict.keys())
-    else:
-      # make a new file, dict...
-      self.count = 0
-      self.nameDict = {}
-
-    self.idx_path = idx_path
-    self.uri      = uri
-
-    if flag is 'a': # create a new one
-      rec_src = mx.recordio.MXIndexedRecordIO(idx_path,uri,'r',key_type)
-      # check consistency...
-      assert len(rec_src.idx.keys()) == len(self.nameDict.keys()),\
+        if flag is 'a': # create a new one
+            rec_src = mx.recordio.MXIndexedRecordIO(idx_path,uri,'r',key_type)
+        # check consistency...
+            assert len(rec_src.idx.keys()) == len(self.nameDict.keys()),\
                'exptected equal, got %d vs %d'%(len(rec_src.idx.keys()) , len(self.nameDict.keys()))
       # make a new one
-      self.rec = mx.recordio.MXIndexedRecordIO(idx_path+'.tmp',uri+'.tmp','w',key_type)
+            self.rec = mx.recordio.MXIndexedRecordIO(idx_path+'.tmp',uri+'.tmp','w',key_type)
+
+            print('copy to a new record, please wait...')
+            for idx in rec_src.idx.keys():
+                data = rec_src.read_idx(idx)
+                #print data
+                self.rec.write_idx(idx,data)
+            print('ok.')
+            rec_src.close()
+        # delete the src, rename the new one
+            os.remove(idx_path)
+            os.remove(uri)
+
+        else:# as usual...
+            self.rec = mx.recordio.MXIndexedRecordIO(idx_path,uri,flag,key_type)
+            if flag == 'w':
+                self.nameDict = {}
+                self.count = 0
 
 
-      print('copy to a new record, please wait...')
-      for idx in rec_src.idx.keys():
-        
-        data = rec_src.read_idx(idx)
-        self.rec.write_idx(idx,data)
-
-      print('ok.')
-      rec_src.close()
-       # delete the src, rename the new one
-      os.remove(idx_path)
-      os.remove(uri)
-
-    else:# as usual...
-      self.rec = mx.recordio.MXIndexedRecordIO(idx_path,uri,flag,key_type)
-      if flag == 'w':
-        os.remove(self.cpk_path)
-        self.nameDict = {}
-	self.count = 0
-
-
-  def write_name(self, name, buf):
+    def write_name(self, name, buf):
      # allocate a ID then, call write_idx
-     if name in self.nameDict: # override
-       self.rec.write_idx(self.nameDict[name],buf)
-     else:
-       self.nameDict[name] = self.count
-       self.rec.write_idx(self.count, buf)
-       self.count += 1
+        if name in self.nameDict: # override
+            self.rec.write_idx(self.nameDict[name],buf)
+        else:
+            self.nameDict[name] = self.count
+            self.rec.write_idx(self.count, buf)
+            self.count += 1
  
-  def read_name(self,name):
-     assert name in self.nameDict,'%s does not exist in Dict!'%name
-     return self.rec.read_idx(self.nameDict[name])
+    def read_name(self,name):
+        assert name in self.nameDict,'%s does not exist in Dict!'%name
+        return self.rec.read_idx(self.nameDict[name])
  
-  def close(self):
+    def close(self):
      # dunmp the dict
-     import cPickle as cpk
-     f = open(self.cpk_path,'w')
-     cpk.dump(self.nameDict,f)
-     f.close()
-     self.rec.close()
-     if os.path.isfile(self.idx_path+'.tmp'): # rename the new one
-       os.rename(self.idx_path+'.tmp',self.idx_path)
-       os.rename(self.uri+'.tmp', self.uri)
+        import cPickle as cpk
+        f = open(self.cpk_path,'w')
+        cpk.dump(self.nameDict,f)
+        f.close()
+        self.rec.close()
+        if os.path.isfile(self.idx_path+'.tmp'): # rename the new one
+            os.rename(self.idx_path+'.tmp',self.idx_path)
+            os.rename(self.uri+'.tmp', self.uri)
 
-  def __del__(self):
-    self.close()
+    def __del__(self):
+        self.close()
 
 
 if __name__  == '__main__':
@@ -123,11 +120,12 @@ if __name__  == '__main__':
   rec.close()
   
   rec=nameRecordIO('test.idx','test.rec','r')
-  assert rec.read_name(imgname1) == d1
+  #print rec.nameDict
+  assert rec.read_name(imgname1) == d1, rec.read_name(imgname1)
   
-  assert rec.read_name(imgname2) == d2
+  assert rec.read_name(imgname2) == d2, rec.read_name(imgname2)
   
-  assert rec.read_name(imgname3) == d3
+  assert rec.read_name(imgname3) == d3, rec.read_name(imgname3)
   assert rec.read_name(imgname4) == d4
   
   rec.close()
